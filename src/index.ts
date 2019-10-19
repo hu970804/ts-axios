f984bdc0194f4c060f84120d3e3b39ebbecb4f0c
hu970804 <812510003@qq.com>
2019-10-19 13:54:32 +0800

    feat: 处理请求 url 参数

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,27 @@
 import { AxiosRequestConfig } from './types'
 import xhr from './xhr'
+import { buildURL } from './helpers/url'
 
 function axios(config: AxiosRequestConfig): void {
+  processConfig(config)
   xhr(config)
+}
+
+/**
+ * 处理 config
+ * @param config
+ */
+function processConfig(config: AxiosRequestConfig): void {
+  config.url = transformURL(config)
+}
+
+/**
+ * 处理 URL
+ * @param config 
+ */
+function transformURL(config: AxiosRequestConfig): string {
+  const { url, params } = config
+  return buildURL(url, params)
 }
 
 export default axios
