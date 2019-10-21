ffce857d054789f04eee806e0e719a74b1d5f0bf
hu970804 <812510003@qq.com>
2019-10-21 16:39:42 +0800

    feat: 错误信息增强

--- src/xhr.ts
+++ src/xhr.ts
@@ -1,5 +1,6 @@
 import { AxiosRequestConfig, AxiosPromise, AxiosResponse } from './types'
 import { parseHeaders } from './helpers/headers'
+import { createError } from './helpers/error'
 
 export default function xhr(config: AxiosRequestConfig): AxiosPromise {
   return new Promise((resolve, reject) => {
@@ -50,12 +51,22 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
 
     // 网络异常错误监听
     request.onerror = function handleError() {
-      reject(new Error('Network Error'))
+      reject(createError(
+        'Network Error',
+        config,
+        null,
+        request
+      ))
     }
 
     // 请求超时错误监听
     request.ontimeout = function handleTimeout() {
-      reject(new Error(`Timeout of ${timeout} ms exceeded`))
+      reject(createError(
+        `Timeout of ${config.timeout} ms exceeded`,
+        config,
+        'ECONNABORTED',
+        request
+      ))
     }
 
     // 非200状态码错误监听   
@@ -63,7 +74,13 @@ export default function xhr(config: AxiosRequestConfig): AxiosPromise {
       if (response.status >= 200 && response.status < 300) {
         resolve(response)
       } else {
-        reject(new Error(`Request failed with status code ${response.status}`))
+        reject(createError(
+          `Request failed with status code ${response.status}`,
+          config,
+          null,
+          request,
+          response
+        ))
       }
     }
   })
